23f98f65ee4bb6ca1fbdbdff71f34707c6f9792f
Wilbelison Junior <meustudio@gmail.com>
2024-09-10 17:43:49 -0300

    post route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,24 @@
 import fastify from 'fastify'
+import { createGoal } from '../functions/create-goal'
+import z from 'zod'
 
 const app = fastify()
 
 const PORT = 3333
+
+app.post('/goals', async request => {
+  const createGoalSchema = z.object({
+    title: z.string(),
+    desiredWeeklyFrequency: z.number().int().min(1).max(7),
+  })
+
+  const body = createGoalSchema.parse(request.body)
+
+  await createGoal({
+    title: body.title,
+    desiredWeeklyFrequency: body.desiredWeeklyFrequency,
+  })
+})
 
 app.listen({ port: PORT }).then(() => {
   console.log(`HTTP server running on http://localhost:${PORT}`)
